6a1d3799230974634f319ab6c54ad10a84ef2857
Fachmieee <fachmieeee@gmail.com>
2021-06-06 19:49:10 +0700

    Initial commit

---
+++ android/app/src/main/java/com/flutter/container/MainActivity.java
@@ -0,0 +1,6 @@
+package com.flutter.container;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello"),
+        ),
+        body: Container(
+          color: Colors.redAccent,
+          margin: EdgeInsets.fromLTRB(10, 15, 20, 25),
+          padding: EdgeInsets.only(bottom: 20, top: 20),
+          child: Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: <Color>[
+                  Colors.amber,
+                  Colors.blue
+                ])),
+          ),
+        )
+      ),
+    );
+  }
+}
